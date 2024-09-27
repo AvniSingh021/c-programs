077ccdfc555f9f953a833b2e7844e623193a0dcc
AvniSingh021 <avnisingh2818@gmail.com>
2024-09-27 15:44:37 +0530

    Add files via upload

---
+++ aarushi.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+    voide main()
+    {
+    int s=0,num ;
+    printf("enter number")
+    scanf("%d" , & num);
+    s=s=num;
+    printf("enter number")
+    scanf("%d" , & num);
+    s=s=num;
+    printf("enter number")
+    scanf("%d" , & num);
+    s=s=num;
+    printf("enter number")
+    scanf("%d" , & num);
+    s=s=num;
+    printf("enter number")
+    scanf("%d" , & num);
+    s=s=num;
+    printf("enter number")
+    scanf("%d" , & num);
+    s=s=num;
+    printf("enter number")
+    scanf("%d" , & num);
+    s=s=num;
+    printf("enter number")
+    scanf("%d" , & num);
+    s=s=num;
+    printf("enter number")
+    scanf("%d" , & num);
+    s=s=num;
+    printf("enter number")
+    scanf("%d" , & num);
+    s=s=num;
+    printf("\n sum=%d"s);
+    return 0;
+    }
+   
+   
+   
+    
